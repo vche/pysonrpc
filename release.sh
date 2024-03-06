aa900db53a702ad13eda2fd2028e66fca491e8ad
Vivien Chene <viv.chene@gmail.com>
2024-03-06 11:50:34 +0000

    Fix coverage workflow

--- release.sh
+++ release.sh
@@ -2,14 +2,6 @@
 echo "--> Updating version to $1"
 echo "__version__ = '$1'" > src/pysonrpc/version.py
 
-echo "--> Building wheel version $1"
-pixi run release
-
-if [ $? -ne 0 ]; then 
-  echo "The command failed with exit status $?" 
-  exit 1 
-fi
-
 echo "--> Pushing commit and generating github release v$1"
 igit up -m "Update release to $1"
 if [ $? -ne 0 ]; then 
@@ -19,6 +11,14 @@ fi
 
 echo "--> Generating github release v$1"
 gh release create v$1 --generate-notes
+if [ $? -ne 0 ]; then 
+  echo "The command failed with exit status $?" 
+  exit 1 
+fi
+
+echo "--> Building packages version $1"
+pixi run release
+
 if [ $? -ne 0 ]; then 
   echo "The command failed with exit status $?" 
   exit 1 
